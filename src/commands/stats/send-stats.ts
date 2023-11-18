325d6c4022558dd3f85e5c2ef3cc422276b93eee
David Luo <Daviedluoo@gmail.com>
2023-11-18 02:52:02 -0500

    Update send-stats
    Bot now sends "data.csv" containing the data in a similar format to the output of the OCR program

--- src/commands/stats/send-stats.ts
+++ src/commands/stats/send-stats.ts
@@ -70,49 +70,88 @@ async function statsCommand(dm: DMChannel) {
     const sessions = results.data.privateBattleHistories.historyGroups.nodes;
 
     const matches = sessions[0].historyDetails.nodes;
-    const verification_strings = matches.map(match => {
-        return match.playedTime + " : " + match.vsRule.name + " " + match.vsStage.name + " : " + match.judgement;
-    });
+    console.log(matches[0]);
+    console.log((await splatnet.getBattleHistoryDetail(matches[0].id)).data.vsHistoryDetail);
+    let csvString = "Timer, Map, Mode, Team 1 Score, Team2 Score, P1 Splashtag, P1 Weapon, P1 KA, P1 Assists, P1 Deaths, P1 Special, P1 #Specials, P1 Paint, P2 Splashtag, P2 Weapon, P2 KA, P2 Assists, P2 Deaths, P2 Special, P2 #Specials, P2 Paint, P3 Splashtag, P3 Weapon, P3 KA, P3 Assists, P3 Deaths, P3 Special,P3 #Specials, P3 Paint, P4 Splashtag, P4 Weapon, P4 KA, P4 Assists, P4 Deaths, P4 Special, P4 #Specials, P4 Paint, P5 Splashtag, P5 Weapon, P5 KA, P5 Assists, P5 Deaths, P5 Special, P5 #Specials, P5 Paint, P6 Splashtag, P6 Weapon, P6 KA, P6 Assists, P6 Deaths, P6 Special, P6 #Specials, P6 Paint, P7 Splashtag, P7 Weapon, P7 KA, P7 Assists, P7 Deaths, P7 Special, P7 #Specials, P7 Paint, P8 Splashtag, P8 Weapon, P8 KA, P8 Assists, P8 Deaths, P8 Special, P8 #Specials, P8 Paint \n";
+    for (let match of matches){
+        let id = match.id;
+        // let timePlayed = match.playedTime;
+        let mode = match.vsRule.name;
+        let stage = match.vsStage.name;
+        // let result = match.judgement;
+        let details = (await splatnet.getBattleHistoryDetail(id)).data.vsHistoryDetail;
+        let my_score = details.myTeam.result?.score
+        let their_score = details.otherTeams[0].result?.score
+        let duration = Math.floor(details.duration / 60) + ":" + details.duration % 60
+        let yourTeamDeets = details.myTeam.players.map(player => {
+            return player.name + "," + player.weapon.name + "," +
+                player.result?.kill + "," + player.result?.death + "," +
+                player.result?.assist + "," + player.weapon.specialWeapon.name +
+                "," + player.result?.special + "," + player.paint;
+        });
+        // console.log(yourTeamDeets[0]);
+        let p1 = "";
+        let p2 = "";
+        let p3 = "";
+        let p4 = "";
+        let p5 = "";
+        let p6 = "";
+        let p7 = "";
+        let p8 = "";
+        if (typeof(yourTeamDeets[0]) != 'undefined'){
+            p1 = yourTeamDeets[0];
+        }else{
+            p1 = ',,,,,,,';
+        }
+        if (typeof(yourTeamDeets[1]) != 'undefined'){
+            p2 = yourTeamDeets[1];
+        }else{
+            p2 = ',,,,,,,';
+        }
+        if (typeof(yourTeamDeets[2]) != 'undefined'){
+            p3 = yourTeamDeets[2];
+        }else{
+            p3 = ',,,,,,,';
+        }
+        if (typeof(yourTeamDeets[3]) != 'undefined'){
+            p4 = yourTeamDeets[3];
+        }else{
+            p4 = ',,,,,,,';
+        }
+        let theirTeamDeets = details.otherTeams[0].players.map(player => {
+            return player.name + "," + player.weapon.name + "," +
+                player.result?.kill + "," + player.result?.death + "," +
+                player.result?.assist + "," + player.weapon.specialWeapon.name +
+                "," + player.result?.special + "," + player.paint;
+        });
+        if (typeof(theirTeamDeets[0]) != 'undefined'){
+            p5 = theirTeamDeets[0];
+        }else{
+            p5 = ',,,,,,,';
+        }
+        if (typeof(theirTeamDeets[1]) != 'undefined'){
+            p6 = theirTeamDeets[1];
+        }else{
+            p6 = ',,,,,,,';
+        }
+        if (typeof(theirTeamDeets[2]) != 'undefined'){
+            p7 = theirTeamDeets[2];
+        }else{
+            p7 = ',,,,,,,';
+        }
+        if (typeof(theirTeamDeets[3]) != 'undefined'){
+            p8 = theirTeamDeets[3];
+        }else{
+            p8 = ',,,,,,,';
+        }
+        csvString += duration + ","+ stage + "," + mode + "," + my_score + "," + their_score + "," + p1 + "," + p2 + "," + p3 + "," + p4 + "," + p5 + "," + p6 + "," + p7 + "," + p8 + "\n";
+        // Format: Timer, Map, Mode, Team1(Winner), Team 1 Score, Team 2(Loser), Team2 Score, P1 Splashtag, P1 Weapon, P1 KA, P1 Assists, P1 Deaths, P1 Specials, P1 Paint, P2...
 
-    let verifyString = ""
-    for (let str of verification_strings) {
-        verifyString += str + "\n"
     }
-    await loadMessage.edit(verifyString);
-    const ids = matches.map(match => { return match.id });
-
-    let deets = (await splatnet.getBattleHistoryDetail(ids[0])).data.vsHistoryDetail;
-    console.log(deets);
-
-    let deets_string = "Your score: " + deets.myTeam.result?.score + "\n";
-    deets_string += "Their score: " + deets.otherTeams[0].result?.score + "\n";
-
-    let duration = Math.floor(deets.duration / 60) + ":" + deets.duration % 60;
-    deets_string += "Match length: " + duration + "\n";
-
-    deets_string += "Your team: \n";
-    let yourTeamDeets = deets.myTeam.players.map(player => {
-        return player.name + ": Weapon: " + player.weapon.name + ", K: " +
-            player.result?.kill + ", D: " + player.result?.death + ", A: " +
-            player.result?.assist + ", special: " + player.weapon.specialWeapon.name +
-            " - " + player.result?.special + ", points: " + player.paint + "p";
-    });
-
-    for (let player of yourTeamDeets) {
-        deets_string += player + "\n";
-    }
-
-    deets_string += "\nTheir team: \n";
-    let theirTeamDeets = deets.otherTeams[0].players.map(player => {
-        return player.name + ": Weapon: " + player.weapon.name + ", K: " +
-            player.result?.kill + ", D: " + player.result?.death + ", A: " +
-            player.result?.assist + ", special: " + player.weapon.specialWeapon.name +
-            " - " + player.result?.special + ", points: " + player.paint + "p";
-    });
-
-    for (let player of theirTeamDeets) {
-        deets_string += player + "\n"
-    }
-
-    await dm.send(deets_string);
+    console.log(csvString)
+    await loadMessage.edit("Done!");
+    const buffer = Buffer.from(csvString, "utf-8");
+    await dm.send(
+        {content: "Here's your data file!",
+         files: [{ attachment: buffer, name: "data.csv"}]});
 }
