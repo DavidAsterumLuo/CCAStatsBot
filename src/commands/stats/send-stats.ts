0cd22deac5e8bb4fa1d882d42642504523334989
David Luo <Daviedluoo@gmail.com>
2023-12-29 19:02:59 -0500

    Update send-stats.ts
    Added some match selection logic
    TODO clean up bot messages and double check injection attacks

--- src/commands/stats/send-stats.ts
+++ src/commands/stats/send-stats.ts
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 
+import { match } from 'assert';
 import { CommandInteraction, DMChannel, Message } from 'discord.js';
+import { sep } from 'path';
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
@@ -74,7 +76,53 @@ async function statsCommand(dm: DMChannel) {
     // console.log((await splatnet.getBattleHistoryDetail(matches[0].id)).data.vsHistoryDetail);
     let csvString = "Timer, Map, Mode, Team 1 Score, Team2 Score, P1 Splashtag, P1 Weapon, P1 KA, P1 Assists, P1 Deaths, P1 Special, P1 #Specials, P1 Paint, P2 Splashtag, P2 Weapon, P2 KA, P2 Assists, P2 Deaths, P2 Special, P2 #Specials, P2 Paint, P3 Splashtag, P3 Weapon, P3 KA, P3 Assists, P3 Deaths, P3 Special,P3 #Specials, P3 Paint, P4 Splashtag, P4 Weapon, P4 KA, P4 Assists, P4 Deaths, P4 Special, P4 #Specials, P4 Paint, P5 Splashtag, P5 Weapon, P5 KA, P5 Assists, P5 Deaths, P5 Special, P5 #Specials, P5 Paint, P6 Splashtag, P6 Weapon, P6 KA, P6 Assists, P6 Deaths, P6 Special, P6 #Specials, P6 Paint, P7 Splashtag, P7 Weapon, P7 KA, P7 Assists, P7 Deaths, P7 Special, P7 #Specials, P7 Paint, P8 Splashtag, P8 Weapon, P8 KA, P8 Assists, P8 Deaths, P8 Special, P8 #Specials, P8 Paint \n";
     // TODO create match selection logic
+
+    // Display first 15 matches
+    let tmp = "";
+    let selectionString = "";
+    let index = 0;
     for (let match of matches){
+        if (index == 15){
+            break;
+        }
+        tmp = "";
+        tmp += match.vsStage.name + ", ";
+        tmp += match.vsRule.name + ", ";
+        tmp += match.judgement;
+        selectionString += String(index) + ": " + tmp + "\n"
+        index ++;
+    }
+    await loadMessage.edit(selectionString);
+    await dm.send("Select matches by responding with 'select (comma seperated numbers)' \nexample: select 0,2,3");
+    // Let user select relevent matches
+    const filter = (m: Message) => m.content.startsWith("select");
+    let selectionMessage = await dm.awaitMessages({ filter, max: 1, time: 600_000, errors: ['time'] }).catch(async (error) => {
+        await dm.send("Response timeout. Please start the process over.");
+        throw error;
+    })
+    
+    let matchSelections = selectionMessage.first()!.content.replace("select", "")
+    matchSelections = matchSelections.replace(/\s/g, '');
+    const regex = /^\d+(,\d+)*$/;
+
+    if (regex.test(matchSelections) == false){
+        await dm.send("message not in correct format, please try again")
+    }
+    const matchIndex = matchSelections.split(',').map(Number);
+    console.log(matchIndex)
+
+    // store relevent matches in 'matches'
+
+
+
+    for (let selectionIndex of matchIndex){
+        let match = matches[0]
+        try {
+            match = matches[selectionIndex]
+        } catch (error) {
+            await dm.send("Invalid Index! Please check index")
+            throw error
+        }
         let id = match.id;
         // let timePlayed = match.playedTime;
         let mode = match.vsRule.name;
@@ -83,7 +131,7 @@ async function statsCommand(dm: DMChannel) {
         let details = (await splatnet.getBattleHistoryDetail(id)).data.vsHistoryDetail;
         let my_score = details.myTeam.result?.score
         let their_score = details.otherTeams[0].result?.score
-        let duration = Math.floor(details.duration / 60) + ":" + details.duration % 60
+        let duration = Math.floor(details.duration / 60) + ":" + String(details.duration % 60).padStart(2, "0")
         let yourTeamDeets = details.myTeam.players.map(player => {
             return player.name +"#"+ player.nameId + "," + player.weapon.name + "," +
                 player.result?.kill + "," + player.result?.death + "," +
@@ -91,33 +139,25 @@ async function statsCommand(dm: DMChannel) {
                 "," + player.result?.special + "," + player.paint;
         });
         // console.log(yourTeamDeets[0]);
-        let p1 = "";
-        let p2 = "";
-        let p3 = "";
-        let p4 = "";
-        let p5 = "";
-        let p6 = "";
-        let p7 = "";
-        let p8 = "";
+        let p1 = ',,,,,,,';
+        let p2 = ',,,,,,,';
+        let p3 = ',,,,,,,';
+        let p4 = ',,,,,,,';
+        let p5 = ',,,,,,,';
+        let p6 = ',,,,,,,';
+        let p7 = ',,,,,,,';
+        let p8 = ',,,,,,,';
         if (typeof(yourTeamDeets[0]) != 'undefined'){
             p1 = yourTeamDeets[0];
-        }else{
-            p1 = ',,,,,,,';
         }
         if (typeof(yourTeamDeets[1]) != 'undefined'){
             p2 = yourTeamDeets[1];
-        }else{
-            p2 = ',,,,,,,';
         }
         if (typeof(yourTeamDeets[2]) != 'undefined'){
             p3 = yourTeamDeets[2];
-        }else{
-            p3 = ',,,,,,,';
         }
         if (typeof(yourTeamDeets[3]) != 'undefined'){
             p4 = yourTeamDeets[3];
-        }else{
-            p4 = ',,,,,,,';
         }
         let theirTeamDeets = details.otherTeams[0].players.map(player => {
             return player.name +"#"+ player.nameId + "," + player.weapon.name + "," +
@@ -127,23 +167,15 @@ async function statsCommand(dm: DMChannel) {
         });
         if (typeof(theirTeamDeets[0]) != 'undefined'){
             p5 = theirTeamDeets[0];
-        }else{
-            p5 = ',,,,,,,';
         }
         if (typeof(theirTeamDeets[1]) != 'undefined'){
             p6 = theirTeamDeets[1];
-        }else{
-            p6 = ',,,,,,,';
         }
         if (typeof(theirTeamDeets[2]) != 'undefined'){
             p7 = theirTeamDeets[2];
-        }else{
-            p7 = ',,,,,,,';
         }
         if (typeof(theirTeamDeets[3]) != 'undefined'){
             p8 = theirTeamDeets[3];
-        }else{
-            p8 = ',,,,,,,';
         }
         // TODO add team names, reference the python script
         csvString += duration + ","+ stage + "," + mode + "," + my_score + "," + their_score + "," + p1 + "," + p2 + "," + p3 + "," + p4 + "," + p5 + "," + p6 + "," + p7 + "," + p8 + "\n";
@@ -151,8 +183,9 @@ async function statsCommand(dm: DMChannel) {
 
     }
     // console.log(csvString)
-    await loadMessage.edit("Done!");
+    // wait loadMessage.edit("Done!");
     const buffer = Buffer.from(csvString, "utf-8");
+    //TODO maybe don't have the players download a file from the bot?
     await dm.send(
         {content: "Here's your data file!",
          files: [{ attachment: buffer, name: "data.csv"}]});
