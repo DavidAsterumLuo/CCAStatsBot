1e14861023ec94dca318266ef985ffda16bed6b5
David Luo <Daviedluoo@gmail.com>
2024-04-01 18:42:06 -0400

    removed kick permisions
    Removed perms

--- src/commands/stats/get-stats.ts
+++ src/commands/stats/get-stats.ts
@@ -6,8 +6,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('get-stats')
-		.setDescription('Retrieves the current CSV for all stats.')
-        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+		.setDescription('Retrieves the current CSV for all stats.'),
 	async execute(interaction: CommandInteraction) {
         for (let weburl of webhookurl){
             let webhook = new WebhookClient({url:weburl})
